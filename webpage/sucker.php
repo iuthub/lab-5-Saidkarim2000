08f7cafd6cf284c7c7afd58a811a163147c7e544
Saidkarim Yuldashev <ysaidkarim@gmail.com>
2021-03-18 16:33:40 +0500

    changes

--- webpage/sucker.php
+++ webpage/sucker.php
@@ -1,15 +1,43 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Buy Your Way to a Better Education!</title>
+		<link href="http://www.cs.washington.edu/education/courses/cse190m/09sp/labs/4-buyagrade/buyagrade.css" type="text/css" rel="stylesheet" />
+	</head>
 
-?>
-</body>
-</html>
+	<body>
+
+		<h1>Thanks, sucker!</h1>
+
+		<p>Your information has been recorded.</p>
+
+		<dl>
+			<dt>Name</dt>
+			<dd><?php $firstname =$_GET['firstname'];
+                echo $firstname ?></dd>
+
+			<dt>Section</dt>
+            <dd><?php
+                $section = $_GET['section'];
+                echo $section
+                ?></dd>
+
+			<dt>Credit Card</dt>
+			<dd><?php
+                $card = $_GET['card'];
+                echo $card;
+
+                ?>
+
+                <?php
+
+                $visa = $_GET['visa'];
+                echo $visa;
+
+                ?>
+
+
+                </dd>
+		</dl>
+	</body>
+</html>  
